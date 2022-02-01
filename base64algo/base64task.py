9f8cf8ea4257d0d4ee156f210d6f3fc830dbab52
AMDEV01 <16246587+amdev01@users.noreply.github.com>
2022-02-01 14:13:53 +0000

    base64algo: Parse arguments to encode or decode a file

--- base64algo/base64task.py
+++ base64algo/base64task.py
@@ -3,18 +3,23 @@ import sys
 import os
 
 usage = """\
-Usage: base64task.py "<file_to_encode>" [-hc] <n>
+Usage: base64task.py [-hced] "<file_to_encode>" <n>
   file_to_encode        - <required> path to the file to encode
   n                     - <optional> number of times to encode file
-  -c/--count n          - <required> if in multi encode mode, <file_to_encode> will be encoded <n> number of times\
+  -e/--encode           - <required> encode <file_to_encode> with base64
+  -d/--decode           - <required> decode <file_to_encode> with base64
+  -c/--count n          - <optional> specify <n> to encode/decode <file_to_encode>\
 """
 
 def encode_test(p_flag):
-    print("Test Base64 encooded once: %s" % base64.b64encode(p_flag)) # print 1st base64 encoding
+    print("Test Base64 encoded once: %s" % base64.b64encode(p_flag)) # print 1st base64 encoding
 
-def encode(file, encode_count):
+def decode_test(p_flag):
+    print("Test Base64 decoded once: %s" % base64.b64decode(p_flag)) # print 1st base64 decoding
+
+def encode(file_to_encode, encode_count):
     # encoding
-    f_s = open(file, "r").read() # get text from file and store it as a string
+    f_s = open(file_to_encode, "r").read() # get text from file and store it as a string
     #b_flag = bytes(f_s, "UTF-8") # convert string to bytes[]
     encoded_flag = base64.b64encode(f_s) # encode flag for the first time
     print("Encoded once")
@@ -22,53 +27,82 @@ def encode(file, encode_count):
         for k in range(encode_count):
             encoded_flag = base64.b64encode(encoded_flag)
             print("Encoded "+ str(k+1) + " times")
-            if (k < 7):
+            if (k < 7): # debug print to see if encoding is correct
               print(encoded_flag.decode('utf8'))
     # create a new file or open existsing one
     try: # try create a new file
-        o_encoded50_f = open("o_encoded50_f.txt", "x")
+        o_encoded_f = open("o_encoded_f.txt", "x")
     except: # open if exists
-        o_encoded50_f = open("o_encoded50_f.txt", "w")
-    o_encoded50_f.write(str(encoded_flag))
-    o_encoded50_f.close()
+        o_encoded_f = open("o_encoded_f.txt", "w")
+    o_encoded_f.write(str(encoded_flag))
+    o_encoded_f.close()
 
 # decoding
-def decode():
-    f_to_decode = open("base64algo/o_encoded50_f.txt")
-    f_s_decode = f_to_decode.read()
+def decode(file_to_decode, decode_count):
+    f_d = open(file_to_decode, "r").read()
+    #f_s_decode = f_to_decode.read()
     #decoded_flag = bytes(f_s_decode, "UTF-8")
 
-    for k in range(50):
-        f_s_decode = base64.b64decode(f_s_decode)
+    for k in range(decode_count):
+        decoded_flag = base64.b64decode(f_d)
 
     #print("Decoded flag: %s " % f_to_decode.decode('utf8'))
-    print("Decoded flag: %s " % f_s_decode)
-
+    #print("Decoded flag: %s " % f_s_decode)
+    try: # try create a new file
+        o_decoded_f = open("o_decoded_f.txt", "x")
+    except: # open if exists
+        o_decoded_f = open("o_decoded_f.txt", "w")
+    o_decoded_f.write(str(encoded_flag))
+    o_decoded_f.close()
 
 def cli():
     print (sys.argv[1:])
 
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 2: # exit if no arguments
         print(usage)
         sys.exit(1)
-    if sys.argv[1] in ["-h", "--help"]:
+    if sys.argv[1] in ["-h", "--help"]: # print help meny
         print(usage)
         sys.exit(0)
-    if sys.argv[1] in ["-c", "--count"]:
-        print(sys.argv[1])
-        if len(sys.argv) < 3:
+    if sys.argv[1] in ["-e", "--encode"]: # set encoding vars
+        if len(sys.argv) < 3: # check if correct number or args
             print(usage)
             sys.exit(1)
-        print(sys.argv[1])
-        file_name = sys.argv[2]
-        encode_count = int(sys.argv[3])
-    else:
-        file_name = sys.argv[1]
-        encode_count = 1
-    if not os.path.isfile(file_name):
+        if sys.argv[2] in ["-c", "--count"]:
+            print(sys.argv[2])
+            if len(sys.argv) < 4: # check if correct number or args
+               print(usage)
+               sys.exit(1)
+            file_name = sys.argv[3]
+            count = int(sys.argv[4])
+            # pass file name and encode count
+        else:
+            file_name = sys.argv[2]
+            count = 1
+            # one action
+    elif sys.argv[1] in ["-d", "--decode"]: # set decoding vars
+        if len(sys.argv) < 3: # check if correct number or args
+            print(usage)
+            sys.exit(1)
+        if sys.argv[2] in ["-c", "--count"]:
+            print(sys.argv[2])
+            if len(sys.argv) < 4: # check if correct number or args
+               print(usage)
+               sys.exit(1)
+            file_name = sys.argv[3]
+            count = int(sys.argv[4])
+            # pass file name and decode count
+        else:
+            file_name = sys.argv[3]
+            count = 1
+            # one action
+    if not os.path.isfile(file_name): # check if file exists
         print("file not found: %s" % file_name)
         sys.exit(1)
-    encode(file_name, encode_count)
+    if sys.argv[1] in ["-e", "--encode"]:
+        encode(file_name, count) # call encode
+    if sys.argv[1] in ["-d", "--decode"]:
+        decode(file_name, count) # call decode 
 
 if __name__ == '__main__':
     cli()
