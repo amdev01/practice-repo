a85de00e032a99124ea833cf06e2effad3af8c52
AMDEV01 <16246587+amdev01@users.noreply.github.com>
2022-02-01 11:54:12 +0000

    base64algp: add cli, allow to encode file n number of times

--- base64algo/base64task.py
+++ base64algo/base64task.py
@@ -1,38 +1,74 @@
 import base64
+import sys
+import os
 
-# encoding
+usage = """\
+Usage: base64task.py "<file_to_encode>" [-hc] <n>
+  file_to_encode        - <required> path to the file to encode
+  n                     - <optional> number of times to encode file
+  -c/--count n          - <required> if in multi encode mode, <file_to_encode> will be encoded <n> number of times\
+"""
 
-f_s = open("base64algo/flagb64.txt", "r").read() # get text from file and store it as a string
-b_flag = bytes(f_s, "UTF-8") # convert string to bytes[]
-print("Base64 encooded once: %s" % base64.b64encode(b_flag)) # print 1st base64 encoding
+def encode_test(p_flag):
+    print("Test Base64 encooded once: %s" % base64.b64encode(p_flag)) # print 1st base64 encoding
 
-encoded_flag = base64.b64encode(b_flag) # encode flag for the first time
-print("Encoded once")
-for k in range(50):
-    encoded_flag = base64.b64encode(encoded_flag)
-    print("Encoded "+ str(k+1) + " times")
-    if (k < 7):
-        print(encoded_flag.decode('utf8'))
-
-# create a new file or open existsing one
-try: 
-    o_encoded50_f = open("base64algo/o_encoded50_f.txt", "x")
-except:
-    o_encoded50_f = open("base64algo/o_encoded50_f.txt", "w")
-
-o_encoded50_f.write(str(encoded_flag))
-o_encoded50_f.close()
+def encode(file, encode_count):
+    # encoding
+    f_s = open(file, "r").read() # get text from file and store it as a string
+    #b_flag = bytes(f_s, "UTF-8") # convert string to bytes[]
+    encoded_flag = base64.b64encode(f_s) # encode flag for the first time
+    print("Encoded once")
+    if (encode_count > 1):
+        for k in range(encode_count):
+            encoded_flag = base64.b64encode(encoded_flag)
+            print("Encoded "+ str(k+1) + " times")
+            if (k < 7):
+              print(encoded_flag.decode('utf8'))
+    # create a new file or open existsing one
+    try: # try create a new file
+        o_encoded50_f = open("o_encoded50_f.txt", "x")
+    except: # open if exists
+        o_encoded50_f = open("o_encoded50_f.txt", "w")
+    o_encoded50_f.write(str(encoded_flag))
+    o_encoded50_f.close()
 
 # decoding
-f_to_decode = open("base64algo/o_encoded50_f.txt")
-f_s_decode = f_to_decode.read()
-#decoded_flag = bytes(f_s_decode, "UTF-8")
+def decode():
+    f_to_decode = open("base64algo/o_encoded50_f.txt")
+    f_s_decode = f_to_decode.read()
+    #decoded_flag = bytes(f_s_decode, "UTF-8")
 
-for k in range(50):
-    f_s_decode = base64.b64decode(f_s_decode)
+    for k in range(50):
+        f_s_decode = base64.b64decode(f_s_decode)
 
-#print("Decoded flag: %s " % f_to_decode.decode('utf8'))
-print("Decoded flag: %s " % f_s_decode)
-    
+    #print("Decoded flag: %s " % f_to_decode.decode('utf8'))
+    print("Decoded flag: %s " % f_s_decode)
 
 
+def cli():
+    print (sys.argv[1:])
+
+    if len(sys.argv) < 2:
+        print(usage)
+        sys.exit(1)
+    if sys.argv[1] in ["-h", "--help"]:
+        print(usage)
+        sys.exit(0)
+    if sys.argv[1] in ["-c", "--count"]:
+        print(sys.argv[1])
+        if len(sys.argv) < 3:
+            print(usage)
+            sys.exit(1)
+        print(sys.argv[1])
+        file_name = sys.argv[2]
+        encode_count = int(sys.argv[3])
+    else:
+        file_name = sys.argv[1]
+        encode_count = 1
+    if not os.path.isfile(file_name):
+        print("file not found: %s" % file_name)
+        sys.exit(1)
+    encode(file_name, encode_count)
+
+if __name__ == '__main__':
+    cli()
